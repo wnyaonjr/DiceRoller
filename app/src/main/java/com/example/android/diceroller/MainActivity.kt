34fc9b2f433ba689179f996c4c86172fe3f2c0cf
wnyaonjr <wnyaonjr@gmail.com>
2021-05-05 19:36:25 +0800

    add id for text result add id for roll button add reference to roll button and set text programmatically

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.android.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Lets Roll"
     }
 }
